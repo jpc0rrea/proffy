6a60729fd4a2495b55ec9643b8df644e61133316
jpc0rrea <joaopedrocorrea14@gmail.com>
2020-08-19 14:47:13 -0300

    Finishing MVP

---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import { View, ScrollView, Text, TextInput } from "react-native";
+import { BorderlessButton, RectButton } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-community/async-storage";
+
+import PageHeader from "../../components/PageHeader";
+import TeacherItem, { Teacher } from "../../components/TeacherItem";
+import api from "../../services/api";
+
+import styles from "./styles";
+
+function TeacherList() {
+  const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+  const [favorites, setFavorites] = useState<number[]>([]);
+  const [subject, setSubject] = useState("");
+  const [week_day, setWeekDay] = useState("");
+  const [time, setTime] = useState("");
+  const [teachers, setTeachers] = useState([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem("favorites").then((response) => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = favoritedTeachers.map(
+          (teacher: Teacher) => {
+            return teacher.id;
+          }
+        );
+
+        setFavorites(favoritedTeachersIds);
+      }
+    });
+  }
+
+  function handleToggleFiltersVisible() {
+    setIsFiltersVisible(!isFiltersVisible);
+  }
+
+  async function handleFiltersSubmit() {
+    loadFavorites();
+
+    const response = await api.get("classes", {
+      params: { subject: subject.trim(), week_day, time },
+    });
+
+    setIsFiltersVisible(false);
+    setTeachers(response.data);
+  }
+
+  return (
+    <View styles={styles.container}>
+      <PageHeader
+        title="Proffys disponíveis"
+        headerRight={
+          <BorderlessButton onPress={handleToggleFiltersVisible}>
+            <Feather name="filter" size={20} color="#FFF" />
+          </BorderlessButton>
+        }
+      >
+        {isFiltersVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput
+              placeholderTextColor="#c1bccc"
+              style={styles.input}
+              value={subject}
+              onChangeText={(text) => setSubject(text)}
+              placeholder="Qual a matéria?"
+            />
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <TextInput
+                  placeholderTextColor="#c1bccc"
+                  style={styles.input}
+                  value={week_day}
+                  onChangeText={(text) => setWeekDay(text)}
+                  placeholder="Qual o dia?"
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput
+                  placeholderTextColor="#c1bccc"
+                  style={styles.input}
+                  value={time}
+                  onChangeText={(text) => setTime(text)}
+                  placeholder="Qual horário?"
+                />
+              </View>
+            </View>
+
+            <RectButton
+              onPress={handleFiltersSubmit}
+              style={styles.submitButton}
+            >
+              <Text style={styles.submitButtonText}>Filtrar</Text>
+            </RectButton>
+          </View>
+        )}
+      </PageHeader>
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        {teachers.map((teacher: Teacher) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+}
+
+export default TeacherList;
